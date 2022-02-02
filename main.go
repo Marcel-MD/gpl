61274e30ed4fb118ac287906a22422ee0effe3dc
Marcel Vlasenco <marcelvlasenco@gmail.com>
2022-02-02 11:17:27 +0200

    Add a REPL that prints tokens generated from commands

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/Marcel-MD/gpl/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s!\n",
+		user.Username)
+	fmt.Printf("Feel free to type in commands:\n")
+	repl.Start(os.Stdin, os.Stdout)
+}

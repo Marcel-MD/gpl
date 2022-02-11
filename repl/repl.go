107c25b118359b1e298b6a4837128873988b1794
Marcel Vlasenco <marcelvlasenco@gmail.com>
2022-02-11 14:17:04 +0200

    Update repl

--- repl/repl.go
+++ repl/repl.go
@@ -6,23 +6,40 @@ import (
 	"io"
 
 	"github.com/Marcel-MD/gpl/lexer"
-	"github.com/Marcel-MD/gpl/token"
+	"github.com/Marcel-MD/gpl/parser"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+
 	for {
 		fmt.Fprint(out, PROMPT)
 		scanned := scanner.Scan()
+
 		if !scanned {
 			return
 		}
+
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
